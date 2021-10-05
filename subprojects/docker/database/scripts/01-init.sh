d99539026439a2f0c2bc49d3da80410a1516358c
efildra <filip.drasic@ericsson.com>
2021-10-05 08:32:08 +0200

    Create docker-compose file for PostgreSQL

---
+++ subprojects/docker/database/scripts/01-init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#############################################################################
+# Bash Shell script to execute psql commands to create fitbitconnect database
+#############################################################################
+
+set -e
+export PGPASSWORD=$POSTGRES_PASSWORD;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASSWORD';
+  CREATE DATABASE $APP_DB;
+  GRANT ALL PRIVILEGES ON DATABASE $APP_DB TO $APP_DB_USER;
+EOSQL
